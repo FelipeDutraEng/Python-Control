aa0d8500916e3b8d07c2fe88eb0c161e8af84bcb
Felipe Dutra <72906430+Fedutr@users.noreply.github.com>
2024-12-04 00:44:11 -0300

    Add files via upload

--- ex071.py
+++ ex071.py
@@ -1,29 +1,14 @@
 print('='*30)
 print('           BANCO CEV')
 print('='*30)
-cedula50 = cedula20 = cedula10 = cedula1 = 0
+cedulas = [50, 20, 10, 1]
+resultado = {}
 valor = int(input('Que valor você quer sacar? R$ '))
-if valor // 50:
-    cedula50 = valor // 50
-    valor -= cedula50 * 50
-if valor != 0:
-    if valor // 20:
-        cedula20 = valor // 20
-        valor -= cedula20 * 20
-    if valor != 0:
-        if valor // 10:
-            cedula10 = valor // 10
-            valor -= cedula10 * 10
-        if valor != 0:
-            if valor // 1:
-                cedula1 = valor
-if cedula50 > 0:
-    print(f'Total de {cedula50} cédulas de R$50')
-if cedula20 > 0:
-    print(f'Total de {cedula20} cédulas de R$20')
-if cedula10 > 0:
-    print(f'Total de {cedula10} cédulas de R$10')
-if cedula1 > 0:
-    print(f'Total de {cedula1} cédulas de R$1')
+for cedula in cedulas:
+    if valor // cedula > 0:
+        resultado[cedula] = valor // cedula
+        valor %= cedula
+for cedula, quantidade in resultado.items():
+    print(f'Total de {quantidade} cédulas de R${cedula}')
 print('='*30)
 print('Volte sempre ao BANCO CEV! Tenha um bom dia!')
