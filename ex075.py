45aae4fc9220388e21c1aedcb0c597779b5aee99
Felipe Dutra <felipeo.dutra@hotmail.com>
2024-12-07 17:30:42 -0300

    atualização de exercicios

--- ex075.py
+++ ex075.py
@@ -1,5 +1,16 @@
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite outro número: '))
-n3 = int(input('Digite mais um número: '))
-n4 = int(input('Digite o último número: '))
-print(f'Você digitou os valores ({n1}, {n2}, {n3}, {n4})')
+numeros = (int(input('Digite um número: ')), int(input('Digite outro número: ')), int(
+    input('Digite mais um número: ')), int(input('Digite o último número: ')))
+print(f'Você digitou os valores ({numeros})')
+print(f'O valor 9 apareceu {numeros.count(9)} vezes')
+if 3 in numeros:
+    print(f'O valor 3 pareceu na {numeros.index(3)+1}º posição')
+else:
+    print('O valor 3 não foi digitado em nenhuma posição')
+pares = [n for n in numeros if n % 2 == 0]
+if pares:
+    print(f'Os valores pares digitados foram ', end='')
+    for n in numeros:
+        if n % 2 == 0:
+            print(n, end=' ')
+else:
+    print('Não possui números pares')
