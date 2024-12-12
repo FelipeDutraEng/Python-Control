d0c5b010ab67e7894edf7b298b52bb3638b67d68
Felipe Dutra <felipeo.dutra@hotmail.com>
2024-12-12 11:13:21 -0300

    Resolução de exercicios

--- ex092.py
+++ ex092.py
@@ -1,2 +1,19 @@
 from datetime import date
-print(date.today().year)
+anoAtual = date.today().year
+dadosAponsentadoria = dict()
+nome = str(input('Nome: ')).strip().title()
+dadosAponsentadoria['nome'] = nome
+nascimento = int(input('Ano de Nascimento: '))
+dadosAponsentadoria['idade'] = anoAtual - nascimento
+ctps = int(input('Carteira de Trabalho (0 não tem): '))
+dadosAponsentadoria['ctps'] = ctps
+if ctps != 0:
+    anoContrato = int(input('Ano de contratação: '))
+    dadosAponsentadoria['contratação'] = anoContrato
+    salário = float(input('Salário: R$ '))
+    dadosAponsentadoria['salário'] = salário
+    dadosAponsentadoria['aposentadoria'] = (
+        35 - (anoAtual - anoContrato)) + dadosAponsentadoria['idade']
+print('-='*30)
+for k, v in dadosAponsentadoria.items():
+    print(f'{k} tem o valor {v}')
---
+++ ex093.py
@@ -0,0 +1,24 @@
+historico = dict()
+gols = list()
+nome = str(input('Nome do Jogador: ')).strip().title()
+partidas = int(input(f'Quantas partidas {nome} jogou? '))
+total = 0
+for j in range(partidas):
+    gol = int(input(f'Quantos gols na partida {j+1}? '))
+    total += gol
+    gols.append(gol)
+
+historico['nome'] = nome
+historico['gols'] = gols
+historico['total'] = total
+print('-='*30)
+print(historico)
+print('-='*30)
+for k, v in historico.items():
+    print(f'O campo {k} tem o valor {v}.')
+print('-='*30)
+print(f'O jogador {historico["nome"]} jogou {
+      len(historico["gols"])} partidas.')
+for j, g in enumerate(gols):
+    print(f'{"=>":>5} Na partida {j+1}, fez {g} gols.')
+print(f'Foi um total de {historico["total"]} gols.')
