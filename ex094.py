9f130a94c6a82c99e3b8fab8bedb846e4a9428c0
Felipe Dutra <felipeo.dutra@hotmail.com>
2024-12-14 21:18:20 -0300

    Correção do exercicio ex094

--- ex094.py
+++ ex094.py
@@ -1,20 +1,24 @@
-temp = dict()
 dados = list()
 soma = 0
+
 while True:
+    temp = dict()
     nome = str(input('Nome: ')).strip().title()
     temp['nome'] = nome
     while True:
         sexo = str(input('Sexo: [M/F] ')).strip().upper()
         if sexo in 'MF':
             break
+        print('Entrada inválida! Por favor, Digite "M" ou "F".')
     temp['sexo'] = sexo
     temp['idade'] = int(input('idade: '))
-    dados.append(temp.copy())
+    # Não precisa de '.copy()' pois estamos recriando o dicionário
+    dados.append(temp)
     soma += temp['idade']
     seguir = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     if seguir in 'N':
         break
+
 print('-='*40)
 print(f'- O grupo tem {len(dados)} pessoas.')
 média = soma / len(dados)
@@ -26,4 +30,5 @@ for pessoa in dados:
 print('- Lista das pessoas que estão acima da média:')
 for pessoa in dados:
     if pessoa["idade"] > média:
-        print(pessoa, end='')
+        print(f'nome = {pessoa["nome"]}; sexo = {
+            pessoa["sexo"]}; idade = {pessoa["idade"]}')
