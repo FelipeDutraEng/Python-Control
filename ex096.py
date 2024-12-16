c31d0b2e7ced0362a51b68fe8149f97037c8387c
Felipe Dutra <felipeo.dutra@hotmail.com>
2024-12-15 23:46:35 -0300

    Exercicios com def()

---
+++ ex096.py
@@ -0,0 +1,25 @@
+def título(msg):
+    print(f'{msg:^30}')
+    print('-'*30)
+
+
+# def área():
+#     largura = float(input('LARGURA (m): '))
+#     comprimento = float(input('COMPRIMENTO (m): '))
+#     área = largura * comprimento
+#     print(f'A área de um terreno {largura}x{comprimento} é de {área:.1f}m²')
+
+
+# título('Controle de Terrenos')
+# área()
+
+def área(largura, comprimento):
+    área = largura * comprimento
+    print(f'A área de um terreno {largura}x{comprimento} é de {área}')
+
+
+título('Controle de Terrenos')
+largura = float(input('LARGURA (m): '))
+comprimento = float(input('COMPRIMENTO (m): '))
+
+área(largura, comprimento)
---
+++ ex097.py
@@ -0,0 +1,12 @@
+def escreva(msg):
+    esp = len(msg)
+    esp += 2
+    print('~'*esp)
+    print(f' {msg:^}')
+    print('~'*esp)
+
+
+escreva('Eu te amo')
+escreva('Gustavo Guanabara')
+escreva('Curso de Python no Toutube')
+escreva('CeV')
