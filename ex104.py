722ce7b5c4bff5fef144cfeb4c6c1d6b1e3e5221
Felipe Dutra <72906430+Fedutr@users.noreply.github.com>
2024-12-23 18:37:05 -0300

    Create ex104.py

---
+++ ex104.py
@@ -0,0 +1,10 @@
+def leiaInt(n)
+  if n != int:
+    print('\033[31mERRO! Digite um número inteiro válido.')
+
+
+
+
+
+n - leiaInt('Digite um número: ')
+print(f'Você acabou de digitar o número{n}')
