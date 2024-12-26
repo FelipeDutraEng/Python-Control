0e181443c481a70042fb0a1cab9787627786b538
Felipe Dutra <72906430+Fedutr@users.noreply.github.com>
2024-12-25 23:52:15 -0300

    atualização

--- ex104.py
+++ ex104.py
@@ -1,7 +1,7 @@
 def leiaInt(n):
   n = 0
   if n != int:
-    return(print('\033[31mERRO! Digite um número inteiro válido.\033[m'))
+    return(print('\033[31mERRO! Digite um número inteiro válido. \033[m'))
 
 
 n = leiaInt('Digite um número: ')
